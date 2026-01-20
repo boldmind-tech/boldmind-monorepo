12a1cc706801f3587c27c7a85c0a5cb649831766
charles <uchecharles223@gmail.com>
2026-01-20 15:10:34 +0100

    deployment check

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -2,13 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Security
-  app.use(helmet);
 
   // CORS
   app.enableCors({
