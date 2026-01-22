84390da4e4b1776fa36319f0070c000759f12607
charles <uchecharles223@gmail.com>
2026-01-22 03:02:44 +0100

    HATE

--- APPS/WEB_APPS/PLANAI_SUITE/planai-landing/postcss.config.js
+++ APPS/WEB_APPS/PLANAI_SUITE/planai-landing/postcss.config.js
@@ -1,10 +1,6 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [
-    require('tailwindcss/nesting'),
-    tailwindcss,
-    autoprefixer,
-  ],
-};
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
