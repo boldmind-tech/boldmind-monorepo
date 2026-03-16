2931d4461b43acf3e24ed40c4d26b7d047404c78
charles <uchecharles223@gmail.com>
2026-03-16 03:10:22 +0100

    fixing deployment after archecture change is opps

--- packages/utils/tsdown.config.ts
+++ packages/utils/tsdown.config.ts
@@ -1,17 +1,23 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: {
+    index:           'src/index.ts',
+    constants:       'src/constants/index.ts',
+    domains:         'src/constants/domains.ts',
+    social:          'src/constants/social-accounts.ts',
+    'database-config': 'src/constants/database-config.ts'
+  },
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
   sourcemap: true,
   minify: false,
+  platform: 'neutral',
+  treeshake: true,
   external: [
     'axios',
     'cookie',
-    'date-fns'
+    'date-fns',
   ],
-  platform: 'neutral',
-  treeshake: true,
 });
